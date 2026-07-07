93b303a5fa7338758eef192a10c6490be639c230
hoaluong <hoaluong@kms-technology.com>
2026-07-07 16:17:16 +0700

    Update datasource URL and simplify AuthorResponse class with Lombok

--- src/main/java/org/example/bookstore/dto/AuthorResponse.java
+++ src/main/java/org/example/bookstore/dto/AuthorResponse.java
@@ -1,8 +1,13 @@
 package org.example.bookstore.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Setter
+@Getter
 public class AuthorResponse {
 
     private Long id;
@@ -13,35 +18,4 @@ public class AuthorResponse {
 
     private List<Long> bookIds = new ArrayList<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public List<Long> getBookIds() {
-        return bookIds;
-    }
-
-    public void setBookIds(List<Long> bookIds) {
-        this.bookIds = bookIds;
-    }
 }
